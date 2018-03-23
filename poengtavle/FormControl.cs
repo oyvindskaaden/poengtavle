9cd31f9b2cb664a2e39b77cdb24ea3135ab7d7f1
Øyvind Skaaden <oyvindskaaden@gmail.com>
2018-03-23 09:37:51 +0100

    Create Second Form

--- poengtavle/FormControl.cs
+++ poengtavle/FormControl.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace poengtavle
 {
-    public partial class Form1 : Form
+    public partial class FormControl : Form
     {
-        public Form1()
+        public FormControl()
         {
             InitializeComponent();
         }
---
+++ poengtavle/FormPoengtavle.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace poengtavle
+{
+    partial class FormPoengtavle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormPoengtavle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(821, 538);
+            this.Name = "FormPoengtavle";
+            this.Text = "Poengtavle";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
